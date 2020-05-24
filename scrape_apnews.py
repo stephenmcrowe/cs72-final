00ded837d7f7113c605efe4028b839a0a2b18780
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-05-24 11:09:02 -0400

    scrape associated press

--- scrape_apnews.py
+++ scrape_apnews.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from pprint import pprint as pp
 import sys
 
-url = "https://abcnews.go.com/Health/Coronavirus"
+url = "https://apnews.com/VirusOutbreak"
 
 # Request
 r1 = requests.get(url)
@@ -14,12 +14,13 @@ r1.status_code
 
 # We'll save in coverpage the cover page content
 coverpage = r1.content
+# print(coverpage)
 
 # Soup creation
 soup1 = BeautifulSoup(coverpage, 'html5lib')
 
 # News identification
-coverpage_news = soup1.find_all(class_='ContentList__Item')
+coverpage_news = soup1.find_all(class_='Component-headline-0-2-111')
 
 number_of_articles = 5
 
@@ -32,11 +33,12 @@ import sys
 for n in range(0, number_of_articles):
         
     # We need to ignore "live" pages since they are not articles
-    if "live" in coverpage_news[n].find('a')['href']:  
+    if "live" in coverpage_news[n]['href']:  
         continue
     
     # Getting the link of the article
-    link = coverpage_news[n].find('a')['href']
+    link = coverpage_news[n]['href']
+    link = "https://apnews.com{}".format(link)
     list_links.append(link)
     
     # Getting the title
@@ -47,7 +49,7 @@ for n in range(0, number_of_articles):
     article = requests.get(link)
     article_content = article.content
     soup_article = BeautifulSoup(article_content, 'html5lib')
-    body = soup_article.find_all('article', class_='Article__Content')
+    body = soup_article.find_all('div', class_='Article')
     x = body[0].find_all('p')
     
     # Unifying the paragraphs
